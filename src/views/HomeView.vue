6b95aa0349e571343580b9452abc77321ab3325c
AnthonyLoche <anthonylocheifc@gmail.com>
2024-07-16 20:49:28 -0300

    FEAT: Criaçao de compontentes/componentização #4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,35 @@
 <script setup>
+import AsideComp from '@/components/Aside/AsideComp.vue'
+import Headercomp from '@/components/Header/HeaderComp.vue'
 </script>
 
 <template>
   <main>
+    <div>
+    <AsideComp/>
+  </div>
+  <div class="right">
+  <div>
+    <Headercomp/>
+  </div>
+  <div class="comp">
+    AQUI VAI O CONTEÚDO
+  </div>
+</div>
   </main>
 </template>
+
+<style scoped>
+main{
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+}
+
+.comp{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+}
+</style>
