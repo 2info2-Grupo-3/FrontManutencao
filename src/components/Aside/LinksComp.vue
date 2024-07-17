bd4c05604f8446fa7c89095f76cd7f5102cdc745
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-07-17 14:54:41 -0300

    chore: asidebar buttons in progress #11

--- src/components/Aside/LinksComp.vue
+++ src/components/Aside/LinksComp.vue
@@ -7,12 +7,15 @@ import { useRoute } from 'vue-router'
 <template>
     <div class="linkButtons">
         <button><img src="../../../public/personicon.svg" alt="">Clientes</button>
+        <button><img src="../../../public/orcamentoicon.svg" alt="">Orçamentos</button>
+        <button><img src="../../../public/faturamentoicon.svg" alt="">Orçamentos</button>
     </div>
 </template>
 
 <style scoped>
 .linkButtons {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 }
@@ -26,7 +29,7 @@ import { useRoute } from 'vue-router'
     color: white;
     padding: .5rem;
     font-size: 16px;
-    margin: 1rem auto;
+    margin: .5rem auto;
     transition: .3s ease-in-out;
 }
 .linkButtons > button:hover{
