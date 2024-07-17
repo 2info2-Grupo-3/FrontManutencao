fc47e4078cae2568c45570a60e0c95e466efffc1
AnthonyLoche <anthonylocheifc@gmail.com>
2024-07-17 20:46:14 -0300

    FEAT: Criação das Stores, Services, exeto orcametos, e criação da plugin

---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+import { PecasService, EntradasService, SaidasService } from "@/services/estoque";
+import { ClientesService, ServicosService} from "@/services/others";
+
+export { PecasService, ClientesService, EntradasService, SaidasService, ServicosService };
---
+++ src/stores/index.js
@@ -0,0 +1,4 @@
+import { usePecasStore, useEntradasStore, useSaidasStore } from "./estoque";
+import { useClientesStore, useServicosStore } from "./others";
+
+export { usePecasStore, useClientesStore, useEntradasStore, useSaidasStore, useServicosStore};
