02889c30a73d8f1aa594a205d94b1ba795491f6f
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-07-22 16:44:11 -0300

    feat: Add modalMain component for form submission #26

---
+++ src/components/ComponentsUtils/modalMain.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  options: Object,
+});
+const handleSubmit = (option) => {
+  console.log('Submit do formulário:', option);
+};
+</script>
+
+<template>
+  <div class="modal">
+    <form @submit.prevent>
+      <div v-for="option in options.inputs" :key="option.id">
+        <input :type="option.type" v-model="option.value">
+        <label>{{ option.label }}</label>
+        <button type="submit" @click="handleSubmit(option)">Enviar</button>
+      </div>
+    </form>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,50 @@
+<template>
+  <main>
+    <div>
+      <AsideComp/>
+    </div>
+    <div class="right">
+      <div>
+        <Headercomp/>
+      </div>
+      <div class="comp">
+        <!-- Ajuste para passar options como uma expressão avaliável -->
+        <modalMain :options="modalOptions" />
+      </div>
+    </div>
+  </main>
+</template>
+
 <script setup>
 import AsideComp from '@/components/Aside/AsideComp.vue'
 import Headercomp from '@/components/Header/HeaderComp.vue'
+import modalMain from '@/components/ComponentsUtils/modalMain.vue'
 import { useOrcamentosStore } from '@/stores'
 import { onMounted } from 'vue'
+
 const store = useOrcamentosStore()
+
+// Defina suas options dentro do script setup
+const modalOptions = {
+  inputs: [
+    { id: 1, type: 'text', value: '', label: 'Nome', prevent: 'minha bola' }
+  ]
+}
+
 onMounted(async () => {
   await store.getOrcamentos()
   console.log(store.state.orcamentos)
 })
 </script>
 
-<template>
-  <main>
-    <div>
-    <AsideComp/>
-  </div>
-  <div class="right">
-  <div>
-    <Headercomp/>
-  </div>
-  <div class="comp">
-    AQUI VAI O CONTEÚDO
-  </div>
-</div>
-  </main>
-</template>
-
 <style scoped>
-main{
+main {
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 5fr;
 }
 
-.comp{
+.comp {
   display: flex;
   justify-content: center;
   align-items: center;
